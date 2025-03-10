0f4c2c673978010cd0245540e8de877870d4a695
Kaur Kivilaan <kaurkivi@gmail.com>
2025-03-10 16:28:08 +0200

    Initiated Spring Boot project

---
+++ src/test/java/com/fujitsu/deliverycostcalc/DeliveryCostCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fujitsu.deliverycostcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryCostCalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
