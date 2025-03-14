165301d209ae2dffbf9b218c49fa0b8f24250854
Kaur Kivilaan <kaurkivi@gmail.com>
2025-03-14 17:40:19 +0200

    Money class instances are now only created from String values for better representation

--- src/main/java/com/fujitsu/deliverycostcalc/Money.java
+++ src/main/java/com/fujitsu/deliverycostcalc/Money.java
@@ -1,27 +1,49 @@
 package com.fujitsu.deliverycostcalc;
 
 public class Money implements Comparable<Money> {
-    private int euros;
     private int cents;
 
-    public Money(int euros, int cents) throws InvalidMoneyException {
-        if (euros < 0) {
-            throw new InvalidMoneyException("Invalid amount of euros (euros can't be negative): " + euros);
-        } else if (cents < 0) {
-            throw new InvalidMoneyException("Invalid amount of cents (cents can't be negative): " + cents);
-        } else if (99 < cents) {
-            throw new InvalidMoneyException("Invalid amount of cents (cents can't be greater than 99): " + cents);
+    public Money(String money) throws InvalidMoneyException {
+        if (money == null || money.isEmpty()) {
+            throw new InvalidMoneyException("Money string cannot be null or empty");
         }
 
-        this.euros = euros;
-        this.cents = cents;
+        money = money.replaceAll("€", "");
+        money = money.trim();
+
+        if (!money.matches("^(((\\d{1,3}(,\\d{3})*|\\d+)(\\.\\d{1,2}))|" +
+                "\\d+(,\\d{1,2})?)0*$")) {
+            throw new InvalidMoneyException("Invalid money format");
+        }
+
+        String[] eurosAndCents;
+        if (money.contains(".")) {
+            money = money.replaceAll(",", "");
+            eurosAndCents = money.split("\\.");
+        } else {
+            eurosAndCents = money.split(",");
+        }
+
+        this.cents = Integer.parseInt(eurosAndCents[0]) * 100;
+
+        if (eurosAndCents.length == 1) {
+            return;
+        }
+
+        String cents = eurosAndCents[1].replaceAll("0+$", "");
+
+        if (cents.length() == 1) {
+            this.cents += Integer.parseInt(cents) * 10;
+        } else {
+            this.cents += Integer.parseInt(cents);
+        }
     }
 
-    public int getInCents() {
-        return 100 * euros + cents;
+    public int getCents() {
+        return cents;
     }
 
     public int compareTo(Money otherMoney) {
-        return Integer.compare(getInCents(), otherMoney.getInCents());
+        return Integer.compare(getCents(), otherMoney.getCents());
     }
 }
