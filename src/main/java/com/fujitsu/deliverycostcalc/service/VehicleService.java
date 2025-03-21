549f819cd5bc9ddf7341d08fc9754f873b8ebde8
Kaur Kivilaan <kaurkivi@gmail.com>
2025-03-21 22:08:08 +0200

    Added a getVehicleTypesAsSet method to VehicleService and its implementor class

--- src/main/java/com/fujitsu/deliverycostcalc/service/VehicleService.java
+++ src/main/java/com/fujitsu/deliverycostcalc/service/VehicleService.java
@@ -3,8 +3,10 @@ package com.fujitsu.deliverycostcalc.service;
 import com.fujitsu.deliverycostcalc.entity.Vehicle;
 import com.fujitsu.deliverycostcalc.repository.VehicleRepository;
 
+import java.util.HashSet;
 import java.util.Optional;
 
 public interface VehicleService extends CrudService<Vehicle, VehicleRepository> {
     Optional<Vehicle> findByType(String type);
+    HashSet<String> getVehicleTypesAsSet();
 }
--- src/main/java/com/fujitsu/deliverycostcalc/service/VehicleServiceImpl.java
+++ src/main/java/com/fujitsu/deliverycostcalc/service/VehicleServiceImpl.java
@@ -4,7 +4,9 @@ import com.fujitsu.deliverycostcalc.entity.Vehicle;
 import com.fujitsu.deliverycostcalc.repository.VehicleRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class VehicleServiceImpl extends CrudServiceImpl<Vehicle, VehicleRepository> implements VehicleService {
@@ -15,5 +17,13 @@ public class VehicleServiceImpl extends CrudServiceImpl<Vehicle, VehicleReposito
     @Override
     public Optional<Vehicle> findByType(String type) {
         return repository.findByType(type);
+    }
+
+    @Override
+    public HashSet<String> getVehicleTypesAsSet() {
+        return findAll()
+                .stream()
+                .map(Vehicle::getType)
+                .collect(Collectors.toCollection(HashSet<String>::new));
     }
 }
