4b93a2c983a57c1306de701a6ec2ad5f1007a8a0
Kaur Kivilaan <kaurkivi@gmail.com>
2025-03-21 15:40:16 +0200

    All repositories extend JpaRepository

--- src/main/java/com/fujitsu/deliverycostcalc/repository/CityRepository.java
+++ src/main/java/com/fujitsu/deliverycostcalc/repository/CityRepository.java
@@ -1,12 +1,12 @@
 package com.fujitsu.deliverycostcalc.repository;
 
 import com.fujitsu.deliverycostcalc.entity.City;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface CityRepository extends CrudRepository<City, Long> {
+public interface CityRepository extends JpaRepository<City, Long> {
     Optional<City> findByStationName(String stationName);
 }
--- src/main/java/com/fujitsu/deliverycostcalc/repository/WeatherDataRepository.java
+++ src/main/java/com/fujitsu/deliverycostcalc/repository/WeatherDataRepository.java
@@ -3,15 +3,15 @@ package com.fujitsu.deliverycostcalc.repository;
 import com.fujitsu.deliverycostcalc.entity.City;
 import com.fujitsu.deliverycostcalc.entity.WeatherData;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface WeatherDataRepository extends CrudRepository<WeatherData, Long> {
+public interface WeatherDataRepository extends JpaRepository<WeatherData, Long> {
     @Query("SELECT w FROM WeatherData w WHERE w.city = :city ORDER BY w.timestamp DESC")
     List<WeatherData> getWeatherDataByCity(@Param("city") City city, Pageable pageable);
 }
